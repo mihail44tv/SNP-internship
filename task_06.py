f4ec56e94f1786908e470561161d6468c4747821
mihail44tv <mihail.sergeew.2OOO@yandex.ru>
2025-06-04 16:20:47 +0300

    Update task_06.py
    Исправил тип значения, возвращаемого функцией

--- task_06.py
+++ task_06.py
@@ -18,12 +18,11 @@ def rps_game_winner(players):
     player_1, move_1 = players[0][0], players[0][1]
     player_2, move_2 = players[1][0], players[1][1]
 
-    if move_1 == move_2:
-        return players[0]
+       if move_1 == move_2:
+        return str(players[0][0]) + ' ' + str(players[0][1])
     elif (move_1 == 'R' and move_2 == 'S') or \
          (move_1 == 'S' and move_2 == 'P') or \
          (move_1 == 'P' and move_2 == 'R'):
-        return players[0]
+        return str(players[0][0]) + ' ' + str(players[0][1])
     else:
-        return players[1]
-
+        return str(players[1][0]) + ' ' + str(players[1][1])
