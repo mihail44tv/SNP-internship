ea852b58d8d8d7f56948828c975010a9d01c6007
mihail44tv <mihail.sergeew.2OOO@yandex.ru>
2025-06-04 16:25:16 +0300

    Update task_06.py

--- task_06.py
+++ task_06.py
@@ -18,7 +18,7 @@ def rps_game_winner(players):
     player_1, move_1 = players[0][0], players[0][1]
     player_2, move_2 = players[1][0], players[1][1]
 
-       if move_1 == move_2:
+    if move_1 == move_2:
         return str(players[0][0]) + ' ' + str(players[0][1])
     elif (move_1 == 'R' and move_2 == 'S') or \
          (move_1 == 'S' and move_2 == 'P') or \
